0d163e66917668cd8cb38a4e164dfa4d356512b5
Lucas Tejedor <lucasbabur@gmail.com>
2023-12-11 17:58:24 -0300

    fix(feedbackcard.tsx): changed import Avatar from @mui/material/avatar to @mui/material/Avatar

--- src/components/home/8Feedback/FeedbackCard.tsx
+++ src/components/home/8Feedback/FeedbackCard.tsx
@@ -5,7 +5,7 @@ import Grid from "@mui/material/Grid";
 import Card from "@mui/material/Card";
 import CardHeader from "@mui/material/CardHeader";
 import CardContent from "@mui/material/CardContent";
-import Avatar from "@mui/material/avatar";
+import Avatar from "@mui/material/Avatar";
 
 interface FeedbackCardProps {
   name: string;
